9f46321844b880284e5c701e955f372d3e217337
Timothy Price <Imnotsaying@gmail.com>
2021-10-06 11:02:07 -0500

    code has errors, working on it

---
+++ iDine/ItemRow.swift
@@ -0,0 +1,28 @@
+//
+//  ItemRow.swift
+//  iDine
+//
+//  Created by Student on 10/6/21.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    let item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            VStack {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
