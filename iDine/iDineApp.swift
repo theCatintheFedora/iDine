3d840cf26baed04a3ada89e866dfbe8bef63e21d
Timothy Price <Imnotsaying@gmail.com>
2021-10-06 10:19:31 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Student on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
